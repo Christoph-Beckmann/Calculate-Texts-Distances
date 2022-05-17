b1b5334e8ad133ade42839d5cae3fffbae55ffbc
Christoph Beckmann <christoph-beckmann@outlook.de>
2022-05-17 16:48:33 +0200

    Changed variable suffix to _T1

--- compare.py
+++ compare.py
@@ -1,11 +1,20 @@
+from time import time
 from rapidfuzz.distance import Levenshtein
 import re
 import pandas as pd
+import time
 
+# Global Variables 
 resfolder = "./res/"
 text1file = "text1.txt"
+lvsth_distance_text1 = 19
 text2file = "text2.txt"
-datafile = "data_1.xlsx"
+lvsth_distance_text2 = 28
+
+# Edit:
+datafile = "data_2.xlsx"
+texts_swapped = True
+
 
 def loadtexts():
     """
@@ -54,7 +63,7 @@ def words(text: str):
     pattern = r"[\w']+|[.,!?;]"
     return re.findall(pattern,text)
 
-def compare_texts(text1: str, text2: str, data: pd.DataFrame, switch: bool ):
+def compare_texts(text1: str, text2: str, data: pd.DataFrame, swapped: bool ):
     """
     Compare each text from data with text1 and text2. Return founded errors. 
 
@@ -68,25 +77,34 @@ def compare_texts(text1: str, text2: str, data: pd.DataFrame, switch: bool ):
     for index, row in data.iterrows():
         # get participant data into variables to pass as parameter in levenshtein function.
         # split each words, and replace empty spaces.
+        participant = row['lfdnr']
+        digital_bool = row['digital_bool']
         pp_text1 = " ".join(row['text1'].split())
         pp_text2 = " ".join(row['text2'].split())
+
+        # create unique VP-Code with lfdnr and digital_bool-value
+        data.loc[index, 'vp'] = int(str(digital_bool)+str(participant))
 
         # Replace participant text with participant text without spaces.
         data.loc[index,'text1'] = pp_text1
         data.loc[index,'text2'] = pp_text2
 
-        if switch == False:
+        if swapped == False:
             lvsth_distance_1 = Levenshtein.distance(words(pp_text1), words(text1))
             lvsth_distance_2 = Levenshtein.distance(words(pp_text2), words(text2))
 
-            data.loc[index,'lvsth_distance1'] = lvsth_distance_1
-            data.loc[index,'lvsth_distance2'] = lvsth_distance_2
+            data.loc[index,'lvsth_distance_T1'] = lvsth_distance_1
+            data.loc[index, 'percent_change_T1'] = ((lvsth_distance_1-lvsth_distance_text1)/lvsth_distance_1)*100
+            data.loc[index,'lvsth_distance_T2'] = lvsth_distance_2
+            data.loc[index, 'percent_change_T2'] = ((lvsth_distance_2-lvsth_distance_text2)/lvsth_distance_2)*100
         else:    # Switch compared text, because we changed texts in week 3. 
-            error_count_1 = Levenshtein.distance(words(pp_text2), words(text1))
-            error_count_2 = Levenshtein.distance(words(pp_text1), words(text2))
+            lvsth_distance_1 = Levenshtein.distance(words(pp_text2), words(text1))
+            lvsth_distance_2 = Levenshtein.distance(words(pp_text1), words(text2))
 
-            data.loc[index,'lvsth_distance1_rev'] = lvsth_distance_1
-            data.loc[index,'lvsth_distance2_rev'] = lvsth_distance_2 
+            data.loc[index,'lvsth_distance_T1_rev'] = lvsth_distance_1
+            data.loc[index, 'percent_change_T1_rev'] = ((lvsth_distance_1-lvsth_distance_text2)/lvsth_distance_1)*100
+            data.loc[index,'lvsth_distance_T2_rev'] = lvsth_distance_2
+            data.loc[index, 'percent_change_T2_rev'] = ((lvsth_distance_2-lvsth_distance_text1)/lvsth_distance_2)*100 
 
     return data
 
@@ -98,7 +116,7 @@ def save_data(new_data: pd.DataFrame):
     """
 
     try:
-        new_data.to_excel(resfolder + 'Calc_error_counts.xlsx')
+        new_data.to_excel(resfolder + 'Calc_error_counts_{}.xlsx'.format(int(time.time())))
     except Exception as error:
         print( "Error in saving new dataset: " + str(error) )
 
@@ -107,5 +125,5 @@ def save_data(new_data: pd.DataFrame):
 if __name__ == "__main__":
     text1, text2 = loadtexts()
     data = loaddata()
-    new_data = compare_texts(text1, text2, data, switch=False) 
+    new_data = compare_texts(text1, text2, data, texts_swapped) 
     save_data(new_data)

6d7681fb723a26a91864a059f6a1fa8f542d7621
Christoph Beckmann <christoph-beckmann@outlook.de>
2023-04-20 12:24:45 +0200

    Improved code and README.md

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# compare_texts
-Comparing texts to identify numerical differences.
+# Text Comparison Tool for Master Thesis
+
+This text comparison tool is designed to assist in the evaluation of texts within the context of a master's thesis. It uses token-based similarity metrics to compare and analyze texts, making it easier to identify differences and similarities between them.
+
+## Table of Contents
+
+1. [Description](#description)
+2. [Requirements](#requirements)
+3. [Installation](#installation)
+4. [Usage](#usage)
+5. [License](#license)
+
+## Description
+
+The main goal of this text comparison tool is to facilitate the analysis of textual data within the context of a master's thesis. It is particularly useful for researchers working with large text datasets, as it provides a streamlined and efficient method for comparing and contrasting textual data.
+
+The tool is implemented in Python and utilizes the `rapidfuzz` library for token-based similarity metrics, allowing for a more accurate comparison of texts. Additionally, it leverages the `pandas` library for efficient data manipulation and the `pathlib` library for easy file management.
+
+## Requirements
+
+- Python 3.6 or higher
+- rapidfuzz
+- pandas
+- pathlib
+
+## Installation
+
+1. Clone this repository or download it as a ZIP file.
+2. Make sure you have Python 3.6 or higher installed on your system.
+3. Install the required libraries by running the following command in your terminal:
+
+```shell
+pip install rapidfuzz pandas
+```
+
+## Usage
+
+1. Place your text files and participant data file (in Excel format) in the res/ folder.
+2. Update the global variables in the script to match your file names and any other settings you'd like to change.
+3. Run the script using the following command in your terminal: `python3 main.py`
+4. The script will generate a new Excel file with the comparison results in the res/ folder.
--- compare.py
+++ compare.py
@@ -1,11 +1,11 @@
-from time import time
-from rapidfuzz.distance import Levenshtein
 import re
 import pandas as pd
-import time
+from time import time
+from rapidfuzz import fuzz
+from pathlib import Path
 
 # Global Variables 
-resfolder = "./res/"
+resfolder = Path("./res/")
 text1file = "text1.txt"
 lvsth_distance_text1 = 19
 text2file = "text2.txt"
@@ -15,115 +15,53 @@ lvsth_distance_text2 = 28
 datafile = "data_2.xlsx"
 texts_swapped = True
 
+def load_texts(file: Path) -> str:
+    with file.open("r") as f:
+        return f.read()
 
-def loadtexts():
-    """
-    Load texts from files.
+def load_data(file: Path) -> pd.DataFrame:
+    return pd.read_excel(
+        file,
+        dtype = {"VP": int, "text1": str, "text2": str}
+    ).reset_index(drop=True)
 
-    :return text1: loaded text 1
-    :return text2: loaded text 2 
-    """
-
-    try:
-        with open(resfolder + text1file,"r") as f:
-            text1 = f.read()
-    except Exception as error:
-        print("Error in loading text 1: " + str(error))
-
-    try:
-        with open(resfolder + text2file, "r") as f:
-            text2 = f.read()
-    except Exception as error:
-        print("Error in loading text 2: " + str(error))
-
-    return text1, text2 
-
-def loaddata():
-    """
-    Load Participant data into dataframe with pandas. 
-
-    :return data: dataframe of participant data
-    """
-
-    try:
-        data = {}
-        data = pd.read_excel(
-            resfolder + datafile,
-            dtype = {"VP": int, "text1": str, "text2": str}
-            )
-    
-        data.reset_index()
-
-    except Exception as error:
-        print("Error in loading dataset: " + str(error))
-
-    return data
-
-def words(text: str):
+def words(text: str) -> list:
     pattern = r"[\w']+|[.,!?;]"
-    return re.findall(pattern,text)
+    return re.findall(pattern, text)
 
-def compare_texts(text1: str, text2: str, data: pd.DataFrame, swapped: bool ):
-    """
-    Compare each text from data with text1 and text2. Return founded errors. 
-
-    :param text1: Correct Text 1 
-    :param text2: Correct Text 2
-    :param data: dataframe of participant data
-
-    :return data: new dataframe
-    """
-
+def compare_texts(text1: str, text2: str, data: pd.DataFrame, swapped: bool) -> pd.DataFrame:
     for index, row in data.iterrows():
-        # get participant data into variables to pass as parameter in levenshtein function.
-        # split each words, and replace empty spaces.
         participant = row['lfdnr']
         digital_bool = row['digital_bool']
         pp_text1 = " ".join(row['text1'].split())
         pp_text2 = " ".join(row['text2'].split())
 
-        # create unique VP-Code with lfdnr and digital_bool-value
         data.loc[index, 'vp'] = int(str(digital_bool)+str(participant))
+        data.loc[index, 'text1'] = pp_text1
+        data.loc[index, 'text2'] = pp_text2
 
-        # Replace participant text with participant text without spaces.
-        data.loc[index,'text1'] = pp_text1
-        data.loc[index,'text2'] = pp_text2
+        if not swapped:
+            lvsth_distance_1 = fuzz.token_set_ratio(words(pp_text1), words(text1))
+            lvsth_distance_2 = fuzz.token_set_ratio(words(pp_text2), words(text2))
+            prefix = ""
+        else:
+            lvsth_distance_1 = fuzz.token_set_ratio(words(pp_text2), words(text1))
+            lvsth_distance_2 = fuzz.token_set_ratio(words(pp_text1), words(text2))
+            prefix = "_rev"
 
-        if swapped == False:
-            lvsth_distance_1 = Levenshtein.distance(words(pp_text1), words(text1))
-            lvsth_distance_2 = Levenshtein.distance(words(pp_text2), words(text2))
-
-            data.loc[index,'lvsth_distance_T1'] = lvsth_distance_1
-            data.loc[index, 'percent_change_T1'] = ((lvsth_distance_1-lvsth_distance_text1)/lvsth_distance_text1)*100
-            data.loc[index,'lvsth_distance_T2'] = lvsth_distance_2
-            data.loc[index, 'percent_change_T2'] = ((lvsth_distance_2-lvsth_distance_text2)/lvsth_distance_text2)*100
-        else:    # Switch compared text, because we changed texts in week 3. 
-            lvsth_distance_1 = Levenshtein.distance(words(pp_text2), words(text1))
-            lvsth_distance_2 = Levenshtein.distance(words(pp_text1), words(text2))
-
-            data.loc[index,'lvsth_distance_T1_rev'] = lvsth_distance_1
-            data.loc[index, 'percent_change_T1_rev'] = ((lvsth_distance_1-lvsth_distance_text2)/lvsth_distance_text2)*100
-            data.loc[index,'lvsth_distance_T2_rev'] = lvsth_distance_2
-            data.loc[index, 'percent_change_T2_rev'] = ((lvsth_distance_2-lvsth_distance_text1)/lvsth_distance_text1)*100 
+        data.loc[index, f'lvsth_distance_T1{prefix}'] = lvsth_distance_1
+        data.loc[index, f'percent_change_T1{prefix}'] = ((lvsth_distance_1-lvsth_distance_text1)/lvsth_distance_text1)*100
+        data.loc[index, f'lvsth_distance_T2{prefix}'] = lvsth_distance_2
+        data.loc[index, f'percent_change_T2{prefix}'] = ((lvsth_distance_2-lvsth_distance_text2)/lvsth_distance_text2)*100
 
     return data
 
-def save_data(new_data: pd.DataFrame):
-    """
-    Saving new dataframe into excel file.
-
-    :param new_data: New dataframe
-    """
-
-    try:
-        new_data.to_excel(resfolder + 'Calc_error_counts_{}.xlsx'.format(int(time.time())))
-    except Exception as error:
-        print( "Error in saving new dataset: " + str(error) )
-
-### Main
+def save_data(new_data: pd.DataFrame, folder: Path):
+    new_data.to_excel(folder / f'Calc_error_counts_{int(time())}.xlsx', index=False)
 
 if __name__ == "__main__":
-    text1, text2 = loadtexts()
-    data = loaddata()
+    text1 = load_texts(resfolder / text1file)
+    text2 = load_texts(resfolder / text2file)
+    data = load_data(resfolder / datafile)
     new_data = compare_texts(text1, text2, data, texts_swapped) 
-    save_data(new_data)
+    save_data(new_data, resfolder)
